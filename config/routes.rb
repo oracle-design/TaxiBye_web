e5c6d5ba9d1120808ae28069acf99b6c94136ed4
FunnyQ <funnyq@funnyq.com>
2017-01-27 02:09:23 +0800

    [add] added routes for basic api

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototype#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :taxis, param: :license_plate_number, only: [] do
+        collection do
+          get 'ranking'
+        end
+        member do
+          post 'query'
+        end
+        resources :trips, only: [:create]
+      end
+    end
+  end
 end
