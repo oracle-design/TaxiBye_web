e41eaa4e51c8a207b980cdf18ea6fdd9dc934a22
FunnyQ <funnyq@funnyq.com>
2017-02-03 04:16:48 +0800

    [add] added login api

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      post :login, to: 'authorization#create'
       resources :taxis, param: :license_plate_number, only: [] do
         collection do
           get 'ranking'
